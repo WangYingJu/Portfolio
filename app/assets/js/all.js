455ebcb2303ef42172de81834317e7d6d9146c2d
“WangYingJu” <iriswang1091@gmail.com>
2023-12-28 17:06:03 +0800

    作品區塊導入isotope.js套件

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -21,3 +21,32 @@ window.addEventListener('DOMContentLoaded', event => {
     });
   });
 });
+
+// Porfolio isotope and filter
+$(window).on('load', function() {
+  var portfolioIsotope = $('.portfolio-container').isotope({
+    itemSelector: '.portfolio-item'
+  });
+
+  $('#portfolio-flters li').on('click', function() {
+    $("#portfolio-flters li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+
+    portfolioIsotope.isotope({
+      filter: $(this).data('filter')
+    });
+  });
+
+  // Initiate venobox (lightbox feature used in portofilo)
+  $(document).ready(function() {
+    $('.venobox').venobox();
+  });
+});
+
+// Portfolio details carousel
+$(".portfolio-details-carousel").owlCarousel({
+  autoplay: true,
+  dots: true,
+  loop: true,
+  items: 1
+});
--- dist/assets/js/all.js
+++ dist/assets/js/all.js
@@ -22,5 +22,29 @@ window.addEventListener('DOMContentLoaded', function (event) {
       }
     });
   });
+}); // Porfolio isotope and filter
+
+$(window).on('load', function () {
+  var portfolioIsotope = $('.portfolio-container').isotope({
+    itemSelector: '.portfolio-item'
+  });
+  $('#portfolio-flters li').on('click', function () {
+    $("#portfolio-flters li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+    portfolioIsotope.isotope({
+      filter: $(this).data('filter')
+    });
+  }); // Initiate venobox (lightbox feature used in portofilo)
+
+  $(document).ready(function () {
+    $('.venobox').venobox();
+  });
+}); // Portfolio details carousel
+
+$(".portfolio-details-carousel").owlCarousel({
+  autoplay: true,
+  dots: true,
+  loop: true,
+  items: 1
 });
 //# sourceMappingURL=all.js.map
