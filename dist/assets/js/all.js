7e53bf589c627253d7399d8410b07d00f466ddad
“WangYingJu” <iriswang1091@gmail.com>
2023-12-28 16:13:53 +0800

    修改meta資訊、footer資訊

--- dist/assets/js/all.js
+++ dist/assets/js/all.js
@@ -1,6 +1,26 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+window.addEventListener('DOMContentLoaded', function (event) {
+  // Activate Bootstrap scrollspy on the main nav element
+  var sideNav = document.body.querySelector('#sideNav');
+
+  if (sideNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: '#sideNav',
+      rootMargin: '0px 0px'
+    });
+  }
+
+  ; // Collapse responsive navbar when toggler is visible
+
+  var navbarToggler = document.body.querySelector('.navbar-toggler');
+  var responsiveNavItems = [].slice.call(document.querySelectorAll('#navbarResponsive .nav-link'));
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener('click', function () {
+      if (window.getComputedStyle(navbarToggler).display !== 'none') {
+        navbarToggler.click();
+      }
+    });
+  });
 });
 //# sourceMappingURL=all.js.map
